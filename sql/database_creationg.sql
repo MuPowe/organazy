b835b2f682f3febf25d0abe39d653b1e31c7539a
Plamen Kostov <kostoww@mail.bg>
2016-08-23 19:41:40 +0300

    fix fonts

---
+++ sql/database_creationg.sql
@@ -0,0 +1,2 @@
+CREATE TABLE `organazy`.`dailymsg` ( `id` INT(3) NOT NULL AUTO_INCREMENT , `msg` VARCHAR(255) NOT NULL , `type` INT(3) NOT NULL , `atype` INT(3) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
