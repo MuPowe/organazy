f19a289387fee6c093776916bab7c8631ae4a193
Plamen Kostov <kostoww@mail.bg>
2016-08-25 23:59:25 +0300

    progress on notes system: connect to mysql

--- process.php
+++ process.php
@@ -1,15 +1,26 @@
 <?php
 include('func/config.php');
+$mysqli = new mysqli(DBHOST,DBUSER, DBPASS, DBDB);
+
+
 
 $post_date = file_get_contents("php://input");
 $data = json_decode($post_date);
+
+$query = 'INSERT INTO `notes` (`id`, `usr_id`, `note`, `type`, `created`, `expired`, `project_id`) VALUES (NULL, \'1\', \'Да не забравя да забравя всичко!\', \'1\', \'\', \'\', \'1\');';
+if ($result = $mysqli->query($query)) {
+
+    $user_info = $result->fetch_assoc();
+    $result->free();
+}
+
 
 
 echo '
 <div ng-controller="" >
       <div class="alert alert-info" id="div2">
-        <i class="fa fa-info-sign"></i><strong>
-        Heads up!</strong> This <a href="components.html#" class="alert-link">alert needs your attention</a>, but it\'s not super important.
+        
+        <?php echo $data->id; ?>
       </div>
 </div>
 ';
